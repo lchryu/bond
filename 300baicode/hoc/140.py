6be7bec401aac889d9a7ac7d6cb727d316c96beb
hello500anhem <hunghuy611613@gmail.com>
2025-10-22 18:00:49 +0700

    tui dep trai vl khakhakhakha

--- 300baicode/hoc/140.py
+++ 300baicode/hoc/140.py
@@ -7,11 +7,11 @@ def is_prime (a) :
     for i in range (2 ,int( (a**0.5))+1) :
         if a % i == 0 :return False
     return True
-c = -999999999999999999999999999999999999999999
-for index in range(a) :
-    if is_prime (index) :
-        if index > c :
-            c = index
-d = b.find(c)            
-print(c ,end=" ")
-print('d') 
+index_prime = -1
+max_prime = -1
+for i in range (a) :
+    if is_prime (b[i]) and b[i] > max_prime :
+        max_prime = b[i]
+        index_prime = i
+print(max_prime ,end= " ")
+if not index_prime == -1 :  print(index_prime)    
--- 300baicode/hoc/tempCodeRunnerFile.py
+++ 300baicode/hoc/tempCodeRunnerFile.py
@@ -1,16 +1,16 @@
-def is_prime (a) :
-    if a < 2 :
-        return False
-    for i in range (2 , int(a **0.5) + 1) :
-        if a % i == 0 :
-            return False
-    return True            
-check = False
 a = int(input())
-b = [int(input()) for i in range (a)]
-for index in range (a) :
-    if is_prime(b[index]):
-        check = True
-        print(index , end = " ")
-if check == False :
-    print("-")
+b = [int(input())for i in range (a)]
+
+def is_prime (a) :
+    if a <= 1 : 
+        return False
+    for i in range (2 ,int( (a**0.5))+1) :
+        if a % i == 0 :return False
+    return True
+index_prime = -1
+max_prime = -1
+for i in range (a) :
+    if is_prime (b[i]) and b[i] > max_prime :
+        max_prime = b[i]
+        index_prime = i
+print(max_prime , index_prime)        
