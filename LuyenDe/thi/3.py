4419e29dfe737b0f8254d0506eb2b8c862b34454
hello500anhem <hunghuy611613@gmail.com>
2026-06-12 09:42:03 +0700

    random commmit

--- LuyenDe/thi/3.py
+++ LuyenDe/thi/3.py
@@ -1,21 +1,15 @@
-def is_true(k,z):
-    return k // 10 + k % 10 == z //10 + z % 10
-a = int(input())
-b = int(input())
-d = int(input())
-c = a * 60 + b
-# e = (c + d) // 1440 // 1440
-e = (c + d) % 1440
-cnt = 0
-while True:
-    h = c // 60
-    m = c % 60
-    if is_true(h,m):
-        cnt += 1
-    if c == e :
-        break
-    c = (c + 1) % 1440
-
-print(cnt)
-    
-
+n = int(input())
+x = int(input())
+y = int(input())
+ans = 0
+for u in range(1, n + 1):
+    for v in range(1, n + 1):
+        if u == x and v == y:
+            continue
+        same_row = (u == x)
+        same_col = (v == y)
+        same_diag = (abs(u - x) == abs(v - y))
+        if same_row or same_col or same_diag:
+            if (u + v) % 2 == 0:
+                ans += 1
+print(ans)
